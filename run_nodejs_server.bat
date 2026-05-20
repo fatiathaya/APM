bce1a5d56fdf6efc90d986c469b26200592cdc48
fatiathaya <fatiathaya@gmail.com>
2026-05-20 20:28:48 +0700

    first commit

--- run_nodejs_server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ========================================
-echo   Starting Node.js Express Server
-echo ========================================
-echo.
-echo Starting server on port 3000...
-echo.
-npm start
